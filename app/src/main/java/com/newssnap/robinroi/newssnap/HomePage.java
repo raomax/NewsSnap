334cf55586e2696ac00fc4a490125646da45271f
raomax <raomax@gmail.com>
2015-01-17 00:46:10 -0500

    Added HomePage Activity. Rough... Very Rough draft

--- app/src/main/java/com/newssnap/robinroi/newssnap/HomePage.java
+++ app/src/main/java/com/newssnap/robinroi/newssnap/HomePage.java
@@ -1,29 +1,24 @@
 package com.newssnap.robinroi.newssnap;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class HomePage extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final Button homeButton = (Button) findViewById(R.id.Home_button);
-        homeButton.setOnClickListener(this);
+        setContentView(R.layout.activity_home_page);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_home_page, menu);
         return true;
     }
 
@@ -41,14 +36,5 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         return super.onOptionsItemSelected(item);
     }
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.Home_button:
-                Intent intent = new Intent(this, HomePage.class);
-                startActivity(intent);
-                finish();
-                break;
-        }
-    }
+
 }
